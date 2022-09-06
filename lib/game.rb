e46af9b36588e6337d900e76e7d2c0a51c5efeed
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-06 22:06:51 +0100

    changed variable names and date

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,17 @@
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :publisher, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date = 'Undefined', publisher = 'Undefined', last_played_at = 'Undefined', archived: false)
+  def initialize(publish_date = 'Undefined', multiplayer = Time.now.strftime('%d/%m/%Y'), last_played_at = 'Undefined', archived: false)
     super(publish_date, archived: archived)
-    @publisher = publisher
+    @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   private
 
   def can_be_archieved?
-    super && @last_played_at > 2
+    super && (Date.today.year - Date.parse(@last_played_at).year) > 2
   end
 end
