bddcd4d7fb37a08e9b2c600e5ef08ce169c6cac1
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-06 22:08:00 +0100

    Fix linter issue

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,8 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date = 'Undefined', multiplayer = Time.now.strftime('%d/%m/%Y'), last_played_at = 'Undefined', archived: false)
+  def initialize(publish_date = 'Undefined', multiplayer = Time.now.strftime('%d/%m/%Y'), last_played_at = 'Undefined',
+                 archived: false)
     super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
