bde70d790a86047d1009d7978eec48e17d1901e0
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-06 20:29:16 +0100

    Add author for readability and writing also defined the add_author funtion as required

--- lib/item.rb
+++ lib/item.rb
@@ -1,13 +1,19 @@
 class Item
-  attr_accessor :publish_date
+  attr_accessor :publish_date, :author
 
   def initialize(publish_date, archived: false)
     @publish_date = publish_date
     @archived = archived
+    @author = nil
   end
 
   def move_to_archive()
     @archived = true if can_be_archieved?
+  end
+
+  def add_author(author)
+    @author = author
+    author.add_item(self)
   end
 
   private
