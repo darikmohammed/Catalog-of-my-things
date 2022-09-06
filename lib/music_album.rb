e7e7a9c178b16c4247f49e993bb81d9c003757a5
dqarias <dino.quispea@gmail.com>
2022-09-06 13:51:22 -0500

    Add can_be_archived method and setter for on_spotify

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -2,9 +2,18 @@ require_relative './item'
 
 class MusicAlbum < Item
 
+    attr_accessor :on_spotify
+
     def initialize(publish_date, archived: false, on_spotify: false)
         super(publish_date, archived: archived)
         @on_spotify = on_spotify
+    end
+
+    private
+
+    def can_be_archieved?
+        return true if super && @on_spotify == true
+        false
     end
 
 end
