ff095f550eb5865a7ddcd5ec331f182e995171d1
darikmohammed <r.m.darik@gmail.com>
2022-09-06 18:26:30 +0300

    Define Label class

---
+++ lib/label.rb
@@ -0,0 +1,12 @@
+class Label
+  attr_accessor :title, :color
+  def initialize(title = 'Unknown', color = 'Unknown')
+    @title = title
+    @color = color
+    @items = items
+  end
+
+  def add_item(item)
+
+  end
+end
