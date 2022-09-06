9a1e54ec5e84ff96f45513c10fd8e501c803db32
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-06 20:43:32 +0100

    changed variable name

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,17 @@
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :publisher, :state
+  attr_accessor :publisher, :last_played_at
 
-  def initialize(publish_date = 'Undefined', publisher = 'Undefined', state = 'Undefined', archived: false)
+  def initialize(publish_date = 'Undefined', publisher = 'Undefined', last_played_at = 'Undefined', archived: false)
     super(publish_date, archived: archived)
     @publisher = publisher
-    @state = state
+    @last_played_at = last_played_at
   end
 
   private
 
   def can_be_archieved?
-    super && @state > 2
+    super && @last_played_at > 2
   end
 end
