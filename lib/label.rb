45b9190448eb09a88d14b14c32f6c7de9f359d5f
darikmohammed <r.m.darik@gmail.com>
2022-09-07 18:07:22 +0300

    Add json addition method for label class

--- lib/label.rb
+++ lib/label.rb
@@ -12,4 +12,15 @@ class Label
     @items.push(item) unless @items.include?(item)
     item.label = self
   end
+  # Serialize Foo object with its class name and arguments
+  def to_json(*args)
+    {
+      JSON.create_id  => self.class.name,
+      'a'             => [ title, color ]
+    }.to_json(*args)
+  end
+  # Deserialize JSON string by constructing new Foo object with arguments.
+  def self.json_create(object)
+    new(*object['a'])
+  end
 end
