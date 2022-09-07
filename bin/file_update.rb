5f72ecc773c31f82b5680988f152fc5e762955fd
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-07 23:40:09 +0100

    add the functions reader

--- bin/file_update.rb
+++ bin/file_update.rb
@@ -4,6 +4,8 @@ module FileUpdate
   def write_json
     File.write('data/book.json', '', mode: 'w')
     File.write('data/label.json', '', mode: 'w')
+    File.write('data/author.json', '', mode: 'w')
+    File.write('data/game.json', '', mode: 'w')
 
     @app.books.each do |book|
       json_book = JSON.generate(book)
@@ -12,6 +14,16 @@ module FileUpdate
     @app.labels.each do |label|
       json_label = JSON.generate(label)
       File.write('data/label.json', "#{json_label}\n", mode: 'a')
+    end
+    
+    @app.authors.each do |author|
+      json_author = JSON.generate(author)
+      File.write('data/author.json', "#{json_author}\n", mode: 'a')
+    end
+
+    @app.games.each do |game|
+      json_game = JSON.generate(game)
+      File.write('data/game.json', "#{json_game}\n", mode: 'a')
     end
   end
 
@@ -37,5 +49,29 @@ module FileUpdate
       labels << JSON.parse(line, create_additions: true)
     end
     labels
+  end
+
+  def reader_author
+    unless File.exist?('data/author.json')
+      File.open('data/author.json', 'w')
+      return []
+    end
+    authors = []
+    File.foreach('data/author.json') do |line|
+      authors << JSON.parse(line, create_additions: true)
+    end
+    authors
+  end
+
+  def reader_game
+    unless File.exist?('data/game.json')
+      File.open('data/game.json', 'w')
+      return []
+    end
+    games = []
+    File.foreach('data/game.json') do |line|
+      games << JSON.parse(line, create_additions: true)
+    end
+    games
   end
 end
