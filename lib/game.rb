018788cefda186409617d34644ef5a1a1156c3c3
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-07 03:19:03 +0100

    Changed the unknown to real date

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,11 @@
 require_relative 'item'
+require 'date'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date = 'Undefined', last_played_at = Time.now.strftime('%d/%m/%Y'), multiplayer: false,
-                 archived: false)
+  def initialize(publish_date = Time.now.strftime('%d/%m/%Y'), last_played_at = Time.now.strftime('%d/%m/%Y'),
+                 multiplayer: false, archived: false)
     super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
