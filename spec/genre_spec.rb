b5d21938ad841958e5c975c04a2da40a56a8bc1f
dqarias <dino.quispea@gmail.com>
2022-09-07 07:23:09 -0500

    Add test for genre class

---
+++ spec/genre_spec.rb
@@ -0,0 +1,48 @@
+require 'rspec'
+require_relative '../lib/item'
+require_relative '../lib/genre'
+
+describe 'Genre' do
+    before do
+        @genre = Genre.new('Action-Adventure')
+    end
+
+    context '#new' do
+        it 'should be an instance of Label' do
+            expect(@genre).to be_instance_of Genre
+        end
+    end
+
+    context '#add_item' do
+        it 'should add item to items array' do
+            item = Item.new('02/01/1988')
+            @genre.add_item(item)
+
+            expect(@genre.items.length).to be 1
+        end
+  
+        it 'should add label to the item object' do
+            item = Item.new('02/01/1988')
+            @genre.add_item(item)
+
+            expect(item.genre).to be @genre
+        end
+    end
+
+    context '#add_genre' do
+        it 'should add genre in item' do
+            item = Item.new('02/01/1988')
+            item.add_genre(@genre)
+
+            expect(item.genre).to be @genre
+        end
+
+        it 'should add item in genre' do
+            item = Item.new('02/01/1988')
+            item.add_genre(@genre)
+
+            expect(@genre.items.length).to eq 1
+        end
+    end
+
+end
