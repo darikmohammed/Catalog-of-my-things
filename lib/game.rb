74ace8c1e2d3700af1f1f4517ac53463d58a515e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-08 01:27:09 +0100

    Add game content to json format but yet to fix a linter issue

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,24 @@ require 'date'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(publish_date = Time.now.strftime('%d/%m/%Y'), last_played_at = Time.now.strftime('%d/%m/%Y'),
-                 multiplayer: false, archived: false)
+  def initialize(publish_date = Time.now.strftime('%d/%m/%Y'),
+                 last_played_at = Time.now.strftime('%d/%m/%Y'),
+                 multiplayer = 'false', archived = 'false') # once
+    # i change the achived to achived: false, it would pass but wont render on the UI
     super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'a' => [publish_date, last_played_at, multiplayer, archived]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(*object['a'])
   end
 
   private
