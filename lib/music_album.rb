ce4a3fa71f58b04be9c6b051c7a018bd007d14af
dqarias <dino.quispea@gmail.com>
2022-09-08 09:47:58 -0500

    Fix music album constructor

--- lib/music_album.rb
+++ lib/music_album.rb
@@ -3,7 +3,7 @@ require_relative './item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date, archived: false, on_spotify: false)
+  def initialize(publish_date, archived = 'false', on_spotify = 'false')
     super(publish_date, archived: archived)
     @on_spotify = on_spotify
   end
